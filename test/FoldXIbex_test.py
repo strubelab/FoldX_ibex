3bd0453c8335ad5b2c1451757f93d6e29963865d
Francisco J. Guzman <36437485+guzmanfj@users.noreply.github.com>
2022-05-05 22:19:39 +0300

    Wrote tests for ibex wrapper

--- test/FoldXIbex_test.py
+++ test/FoldXIbex_test.py
@@ -1,16 +1,12 @@
 import unittest
 
-import os, sys, shutil
+import os, shutil
 import tempfile
 from pathlib import Path
+import pickle
+from FoldX.FoldX_ibex import FoldXIbex
 
-from Bio import SeqIO
-
-sys.path.append(os.fspath(Path(__file__).parent.parent / 'example_wrapper'))
-
-from wrapper_ibex import ProgramIbex
-
-class ProgramIbexTest(unittest.TestCase):
+class FoldXIbexTest(unittest.TestCase):
     """
     Class for testing the Ibex module
     """
@@ -20,69 +16,76 @@ class ProgramIbexTest(unittest.TestCase):
         Example case where there the test input is a fasta file with many
         sequences
         """
-        self.sequences = list(SeqIO.parse(
-            Path(__file__).parent / 'test_outputs' / 'sequences.fasta',
-            'fasta'))
+        self.pdbs = [Path('./test/test_outputs/2oun.pdb')] * 2
+        self.mutations = [[['L675W'], ['L675P']]] * 2
+        self.chains = [['A','B'],['A','B']]
+
         self.tempdir = Path(tempfile.gettempdir(),
             self.__class__.__name__.lower())
         self.out_dir = self.tempdir
         self.out_ibex = self.out_dir / 'out_ibex'
-        self.sequences_dir = self.out_dir / 'sequences'
+        self.pdbs_dir = self.out_dir / 'pdbs'
 
-        self.jobname = 'programibex_unittest'
+        self.jobname = 'foldxibex_unittest'
 
         self.script_file = self.out_ibex / 'script.sh'
         
-        self.programibex = ProgramIbex(self.sequences, out_dir=self.out_dir,
-            tempdir=self.tempdir, jobname=self.jobname)
+        self.exe = FoldXIbex(self.pdbs, self.mutations, self.chains,
+            out_dir=self.out_dir, tempdir=self.tempdir, jobname=self.jobname)
     
 
-    def test_sequences_written(self) -> None:
+    def test_pdbs_mutations_written(self) -> None:
         """
-        Test that the sequence files were creating according to the number of
+        Test that the pickle files were created according to the number of
         sequences per job
         """
-        
-        file_names = [self.sequences_dir / f'sequences{i}.fasta' \
+        file_names = [self.pdbs_dir / f'pdbs{i}.pkl' \
                         for i in range(10)]
-        self.programibex.prepare()
+        self.exe.prepare()
 
         for i, file in enumerate(file_names):
             self.assertTrue(file.exists())
-            seq = SeqIO.read(file, 'fasta')
-            self.assertEqual(str(self.sequences[i].seq), str(seq.seq))
+            with open(file, 'rb') as f:
+                pdb_muts = pickle.load(f)
+
+        pdbs, mutations, chains = list(zip(pdb_muts))
+
+        self.assertEqual(self.pdbs, pdbs)
+        self.assertEqual(self.mutations, mutations)
+        self.assertEqual(self.chains, chains)
+
 
     def test_script_made(self) -> None:
         """
         Test that the script for ibex is made correctly
         """
-        self.programibex.prepare()
+        self.exe.prepare()
 
         self.python_file = (Path(__file__).parent.parent.resolve()
-                                / 'example_wrapper'
+                                / 'FoldX'
                                 / 'run_wrapper.py')
 
         self.script = (
-            '#!/bin/bash\n'
+            '#!/bin/bash --login\n'
             f'#SBATCH --job-name=programibex_unittest\n'
             f'#SBATCH --partition=batch\n'
             f'#SBATCH --output={self.out_ibex}/%J.out\n'
-            f'#SBATCH --time=00:17:00\n'
+            f'#SBATCH --time=00:01:00\n'
             f'#SBATCH --ntasks=1\n'
-            f'#SBATCH --cpus-per-task=4\n'
-            f"#SBATCH --mem-per-cpu=2G\n"
-            f'#SBATCH --array=0-9\n'
+            f'#SBATCH --cpus-per-task=2\n'
+            f"#SBATCH --mem=4G\n"
+            f'#SBATCH --array=0-1\n'
             '\n'
-            f'module load tgt_package'
+            f'conda activate {self.exe.conda_env}'
             '\n'
-            f'seq_file="{self.sequences_dir.resolve()}/'
-            'sequences${SLURM_ARRAY_TASK_ID}.fasta"\n'
+            f'seq_file="{self.pdbs_dir.resolve()}/'
+            'pdbs${SLURM_ARRAY_TASK_ID}.pkl"\n'
             f'python {self.python_file} '
             '${seq_file} '
             f'{self.out_dir}\n'
         )
 
-        self.assertEqual(self.script, self.programibex.script)
+        self.assertEqual(self.script, self.exe.script)
         
         self.assertTrue(self.script_file.exists())
         with open(self.script_file, 'r') as f:
@@ -95,3 +98,5 @@ class ProgramIbexTest(unittest.TestCase):
         if os.path.isdir(self.tempdir):
             shutil.rmtree(self.tempdir)
 
+if __name__ == '__main__':
+    unittest.main()
